95b155750a585892b3e8b33bac373945d959d2eb
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-29 17:02:59 +0000

    Neater test

--- src/test/java/CopierShould.java
+++ src/test/java/CopierShould.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
 import static org.mockito.Mockito.*;
 
@@ -13,7 +11,7 @@ public class CopierShould {
     @BeforeEach
     void setUp() {
         source = mock(Source.class);
-        destination = mock(Destination.class, withSettings().verboseLogging());
+        destination = mock(Destination.class);
     }
 
     @Test
@@ -29,44 +27,13 @@ public class CopierShould {
 
     @Test
     public void copy_characters_until_newline() {
-        String input = "abc\n";
-        String expectedOutput = input.substring(0, input.indexOf('\n'));
-
-        when(source.getChar()).thenAnswer(new Answer() {
-            int i = 0;
-
-            @Override
-            public Object answer(InvocationOnMock invocation) {
-                return input.charAt(i++);
-            }
-        });
+        when(source.getChar()).thenReturn('a', 'b', 'c', 'c', 'd', '\n', 'e', 'f');
 
         new Copier(source, destination).copy();
 
-        var order = inOrder(destination);
-        for (char c: expectedOutput.toCharArray()) {
-            order.verify(destination, times(1)).setChar(c);
-        }
-    }
-
-    class Foo {
-        void call(int i) {}
-    }
-
-    @Test
-    public void how_does_mockito_work() {
-        var obj = mock(Foo.class);
-
-        obj.call(0);
-        obj.call(1);
-        obj.call(2);
-
-        for (int i = 0; i < 3; i++) {
-            verify(obj).call(i);
-        }
-
-
-
-
+        verify(destination).setChar('a');
+        verify(destination).setChar('b');
+        verify(destination, times(2)).setChar('c');
+        verify(destination).setChar('d');
     }
 }
